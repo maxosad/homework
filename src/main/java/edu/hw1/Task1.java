995f895a1fed8d5b92392545d929e38705d7a01e
maxosad <maxosad@mail.ru>
2023-10-12 16:35:24 +0300

    constant 60

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -2,12 +2,13 @@ package edu.hw1;
 
 public class Task1 {
     public static Integer minutesToSeconds(String minutesSeconds) throws IllegalArgumentException{
+        final int SIXTY = 60;
         String[] minSec =  minutesSeconds.split(":");
-        Integer minutes = Integer.parseInt(minSec[0]);
-        Integer seconds = Integer.parseInt(minSec[1]);
-        if (seconds >= 60) {
+        int minutes = Integer.parseInt(minSec[0]);
+        int seconds = Integer.parseInt(minSec[1]);
+        if (seconds >= SIXTY) {
            return -1;
         }
-        return minutes * 60 + seconds;
+        return minutes * SIXTY + seconds;
     }
 }
