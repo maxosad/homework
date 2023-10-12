c62019de4ef1930d12cbe78b464899b5dec95ae5
maxosad <maxosad@mail.ru>
2023-10-12 16:38:23 +0300

    constant 10

--- src/main/java/edu/hw1/Task2.java
+++ src/main/java/edu/hw1/Task2.java
@@ -3,10 +3,10 @@ package edu.hw1;
 public class Task2 {
     public static Integer countDigits(Integer numberToCountDigits) {
         Integer countDigits = 0;
-
+        final int TEN = 10;
         do {
             countDigits++;
-            numberToCountDigits /= 10;
+            numberToCountDigits /= TEN;
         } while (numberToCountDigits > 0);
 
         return countDigits;
