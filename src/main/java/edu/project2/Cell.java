ff9999fc718ca80a23ee92c6a705831e158791fa
maxosad <maxosad@mail.ru>
2023-10-25 17:30:21 +0300

    cosmetic commit

--- src/main/java/edu/project2/Cell.java
+++ src/main/java/edu/project2/Cell.java
@@ -1,5 +1,7 @@
 package edu.project2;
 
+import java.util.Map;
+
 public record Cell(int row, int col, Type type) {
     public enum Type { WALL, PASSAGE }
 }
--- src/main/java/edu/project2/Maze.java
+++ src/main/java/edu/project2/Maze.java
@@ -4,4 +4,10 @@ public final class Maze {
     private final int height;
     private final int width;
     private final Cell[][] grid;
+
+    public Maze(int height, int width) {
+        this.width = width;
+        this.height = height;
+        grid = new Cell[width][height];
+    }
 }
