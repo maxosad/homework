f127748329ff108dfdeb99fb50cf260b2d2f622f
maxosad <maxosad@mail.ru>
2023-10-30 22:02:46 +0300

    Map::megre

--- src/main/java/edu/hw3/Task3.java
+++ src/main/java/edu/hw3/Task3.java
@@ -1,5 +1,6 @@
 package edu.hw3;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,7 +10,7 @@ public class Task3 {
     public static <K> Map<K, Integer> freqDict(K[] arrayOfKeys) {
         Map<K, Integer> ansMap = new HashMap<>();
         for (K el : arrayOfKeys) {
-            ansMap.put(el, ansMap.getOrDefault(el, 0) + 1);
+            ansMap.merge(el, 1, Integer::sum);
         }
         return ansMap;
     }
