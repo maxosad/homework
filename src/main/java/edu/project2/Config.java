0de8323ce6c6fa920147ad4b27de31290a6940c0
maxosad <maxosad@mail.ru>
2023-10-30 10:29:20 +0300

    console input

--- src/main/java/edu/project2/Config.java
+++ src/main/java/edu/project2/Config.java
@@ -7,5 +7,6 @@ public class Config {
 
     public static final int DIRECTIONS_COUNT = 4;
     public static final List<Integer> DIRECTIONS_LIST = List.of(0, 1, 2, 3);
+    public static final int HEIGHT_WIDTH_LIMIT = 3;
 
 }
--- src/main/java/edu/project2/Main.java
+++ src/main/java/edu/project2/Main.java
@@ -2,12 +2,11 @@ package edu.project2;
 
 import edu.project2.Generators.Generator;
 import edu.project2.Generators.GeneratorBridge;
-import edu.project2.Renderers.PrettyRenderer;
 import edu.project2.Renderers.Renderer;
-import edu.project2.Renderers.SimpleRenderer;
-import edu.project2.Solvers.BFSSolver;
-import edu.project2.Solvers.DFSSolver;
+import edu.project2.Renderers.RendererBridge;
 import edu.project2.Solvers.Solver;
+import edu.project2.Solvers.SolverBridge;
+import edu.project2.model.Cell;
 import edu.project2.model.Coordinate;
 import edu.project2.model.GeneratorType;
 import edu.project2.model.Maze;
@@ -15,6 +14,8 @@ import edu.project2.model.RendererType;
 import edu.project2.model.SolverType;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -22,62 +23,83 @@ public class Main {
 
     public final static Logger LOGGER = LogManager.getLogger();
     public static final String BFS = "BFS";
+    public static final String SHOULD_BE_PASSAGE = "should be passage";
+    public static final String SHOULD_BE_MORE_3 = "should be >= 3";
+    private static Scanner scanner;
+    private static Maze maze;
 
     private Main() { }
 
+    private static Coordinate biConditionInputInts(String coordName, BiPredicate<Integer, Integer> biPredicate, String message) {
+        int col;
+        int row;
+        while (true) {
+            LOGGER.info("enter %s row and col, it should be passage".formatted(coordName));
+            row = scanner.nextInt();
+            col = scanner.nextInt();
+            if (biPredicate.test(row, col)) {
+                return new Coordinate(row, col);
+            }
+            LOGGER.info("%s coordinate %s".formatted(coordName, message));
+        }
+    }
+
+    private static <T> T chooseFromValues(Class<T> en, String name) {
+        var ens = en.getEnumConstants();
+        String choosenType;
+        while (true) {
+            LOGGER.info("Which %s do you want to choose?".formatted(name));
+            for (var type : ens) {
+                LOGGER.info(type);
+            }
+            choosenType = scanner.nextLine();
+            for (var type : ens) {
+                if (type.toString().equals(choosenType)) {
+                    return type;
+                }
+            }
+            LOGGER.info("you have to choose %s from presented values".formatted(name));
+        }
+    }
+
+    private static int conditionInputInt(String name, Predicate<Integer> predicate, String message) {
+        int input;
+        while (true) {
+            LOGGER.info("enter %s".formatted(name));
+            input = scanner.nextInt();
+            if (predicate.test(input)) {
+                return input;
+            }
+            LOGGER.info("%s %s".formatted(name, message));
+        }
+    }
+
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean f = false;
+        scanner = new Scanner(System.in);
+        Generator generator = new GeneratorBridge(chooseFromValues(GeneratorType.class, "generator"));
 
-        LOGGER.info("Which generator do you want to choose?");
-        for (var type : GeneratorType.values()) {
-            LOGGER.info(type.toString());
-        }
-        String generatorType = scanner.nextLine();
-        Generator generator =
-            new GeneratorBridge(generatorType.equals(BFS) ? GeneratorType.BFS : GeneratorType.PRIM);
+        int initHeight = conditionInputInt("height", x -> x >= 3, SHOULD_BE_MORE_3);
+        int initWidth = conditionInputInt("width", x -> x >= 3, SHOULD_BE_MORE_3);
 
-        LOGGER.info("enter initial height >= 3");
-        int initHeight = scanner.nextInt();
+        int seed = conditionInputInt("seed", x -> true, "");
 
-        LOGGER.info("enter initial width >= 3");
-        int initWidth = scanner.nextInt();
-
-        LOGGER.info("enter seed");
-        int seed = scanner.nextInt();
-
-        Maze maze = generator.generate(initHeight, initWidth, seed);
-
-        LOGGER.info("Which renderer do you want to choose?");
-        for (var type : RendererType.values()) {
-            LOGGER.info(type.toString());
-        }
         scanner.nextLine();
-        String rendererType = scanner.nextLine();
-        Renderer renderer = rendererType.equals("SIMPLE") ? new SimpleRenderer() : new PrettyRenderer();
+        maze = generator.generate(initHeight, initWidth, seed);
+        Renderer renderer = new RendererBridge(chooseFromValues(RendererType.class, "renderer"));
         LOGGER.info(renderer.render(maze));
 
-        LOGGER.info("Which solver do you want to choose?");
-        for (var type : SolverType.values()) {
-            LOGGER.info(type.toString());
-        }
-        String solverType = scanner.nextLine();
-        Solver solver = solverType.equals(BFS) ? new BFSSolver() : new DFSSolver();
+        Solver solver = new SolverBridge(chooseFromValues(SolverType.class, "solver"));
 
-        LOGGER.info("enter start row");
-        int startRow = scanner.nextInt();
-
-        LOGGER.info("enter start col");
-        int startCol = scanner.nextInt();
-
-        Coordinate start = new Coordinate(startRow, startCol);
-
-        LOGGER.info("enter end row");
-        int endRow = scanner.nextInt();
-
-        LOGGER.info("enter end col");
-        int endCol = scanner.nextInt();
-        Coordinate end = new Coordinate(endRow, endCol);
+        Coordinate start =
+            biConditionInputInts("start",
+                (row, col) -> maze.getGrid()[row][col].getType().equals(Cell.Type.PASSAGE),
+                SHOULD_BE_PASSAGE
+            );
+        Coordinate end =
+            biConditionInputInts("end",
+                (row, col) -> maze.getGrid()[row][col].getType().equals(Cell.Type.PASSAGE),
+                SHOULD_BE_PASSAGE);
 
         List<Coordinate> answer = solver.solve(maze, start, end);
         LOGGER.info(renderer.render(maze, answer));
