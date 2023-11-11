dddce8bd938ba19a713b95b2dbeafb69882907ff
maxosad <maxosad@mail.ru>
2023-11-11 14:58:57 +0300

    args[]

--- src/main/java/edu/project3/Main.java
+++ src/main/java/edu/project3/Main.java
@@ -1,6 +1,14 @@
 package edu.project3;
 
+import java.util.Arrays;
+
 public class Main {
+//    public static void main(String[] args) {
+//        System.out.println(Arrays.toString(args));
+//        //    java -jar build.jar --path logs/2023* --from 2023-08-31 --format markdown
+////[--path, logs/2023*, --from, 2023-08-31, --format, markdown]
+//    }
+
     public static void main(String[] args) {
 
     }
