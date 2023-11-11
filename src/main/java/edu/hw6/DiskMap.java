a7a5c52971f3ccf115e8b3d8649e328ee2b843e6
maxosad <maxosad@mail.ru>
2023-11-11 18:08:16 +0300

    put

--- src/main/java/edu/hw6/DiskMap.java
+++ src/main/java/edu/hw6/DiskMap.java
@@ -2,24 +2,22 @@ package edu.hw6;
 
 import java.io.EOFException;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.nio.file.Path;
 import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class DiskMap extends AbstractMap<String, String> implements Map<String, String> {
     private final static String PATH = "src/main/java/edu/hw6/Disk.txt";
-//    private final static String PATH = "Disk.txt";
 
     public static void main(String[] args) {
-        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(PATH)))
-        {
+        HashMap<String, String> m = new HashMap<>();
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(PATH))) {
             AbstractMap.SimpleEntry<String, String> entry = new SimpleEntry<>("maxim", "osadchiy");
             oos.writeObject(entry);
             AbstractMap.SimpleEntry<String, String> entry1 = new SimpleEntry<>("maxim1", "osadchiy2");
@@ -27,21 +25,23 @@ public class DiskMap extends AbstractMap<String, String> implements Map<String, 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        DiskMap m = new DiskMap();
-        System.out.println(m.entrySet());
+        DiskMap mm = new DiskMap();
+        System.out.println(mm.entrySet());
     }
     @Override
     public Set<Entry<String, String>> entrySet() {
         Set<Entry<String, String>> diskSet = new HashSet<>();
+//        Set<Entry<String, String>> diskSet = new EntrySet<>();
         try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(PATH))) {
             boolean f = true;
             while (f) {
                 try {
-                    var o = ois.readObject();
                     AbstractMap.SimpleEntry<String, String> p =
-                        (AbstractMap.SimpleEntry<String, String>) o;
+                        (AbstractMap.SimpleEntry<String, String>) ois.readObject();
                     diskSet.add(p);
-                }catch (EOFException ignore) {f = false; }
+                } catch (EOFException ignore) {
+                    f = false;
+                }
 
             }
 //            System.out.println(p.toString());
@@ -53,6 +53,25 @@ public class DiskMap extends AbstractMap<String, String> implements Map<String, 
 
     @Override
     public String put(String key, String value) {
-        return null;
+        var entrySet = this.entrySet();
+        AbstractMap.SimpleEntry<String, String> newEntry = new AbstractMap.SimpleEntry<>(key, value);
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(PATH))) {
+            boolean existedKey = true;
+            for (var entry : entrySet) {
+                if (entry.getKey().equals(key)) {
+                    oos.writeObject(newEntry);
+                    existedKey = false;
+                } else {
+                    oos.writeObject(entry);
+                }
+            }
+            if (existedKey) {
+                oos.writeObject(newEntry);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return value;
     }
 }
+
---
+++ src/test/java/edu/hw6/DiskMapTest.java
@@ -0,0 +1,14 @@
+package edu.hw6;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiskMapTest {
+    private Map<String, String> a;
+
+    @Test
+    void put() {
+    }
+}
