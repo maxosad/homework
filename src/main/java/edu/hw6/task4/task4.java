e78efe79b1e5039a5951ed92b4c374cc84959e34
maxosad <maxosad@mail.ru>
2023-11-12 23:34:03 +0300

    task3 task4

---
+++ src/main/java/edu/hw6/task4/task4.java
@@ -0,0 +1,33 @@
+package edu.hw6.task4;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.zip.Adler32;
+import java.util.zip.CheckedOutputStream;
+
+public class task4 {
+    public static void main(String[] args) {
+//        PrintWriter -> OutputStreamWriter -> BufferedOutputStream
+//            -> CheckedOutputStream -> file OutputStream.
+        Path file = Path.of("src/main/java/edu/hw6/task4/files/input.txt");
+        try (var oos = Files.newOutputStream(file)){
+            try (CheckedOutputStream cos = new CheckedOutputStream(oos, new Adler32())) {
+                try (BufferedOutputStream bos = new BufferedOutputStream(cos)) {
+                    try (OutputStreamWriter osw = new OutputStreamWriter(bos, StandardCharsets.UTF_8)) {
+                        try (PrintWriter pw = new PrintWriter(osw)) {
+                            osw.write("Programming is learned by writing programs. ― Brian Kernighan");
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
