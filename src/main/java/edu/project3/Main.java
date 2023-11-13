52d8b85f97eb6161ec8327c2a348b0f86bf5e042
maxosad <maxosad@mail.ru>
2023-11-13 20:16:56 +0300

    OutputFormat

--- src/main/java/edu/project3/Main.java
+++ src/main/java/edu/project3/Main.java
@@ -1,6 +1,11 @@
 package edu.project3;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
 //    public static void main(String[] args) {
@@ -9,7 +14,12 @@ public class Main {
 ////[--path, logs/2023*, --from, 2023-08-31, --format, markdown]
 //    }
 
+    private Main() { }
+    public static Set<String> KEYS = new HashSet<>(List.of("--path", "--from", "--to", "--format"));
+    public static final Logger LOGGER = LogManager.getLogger();
+
     public static void main(String[] args) {
+        LOGGER.info(KEYS.contains("--to"));
 
     }
 }
---
+++ src/main/java/edu/project3/OutputFormat.java
@@ -0,0 +1,8 @@
+package edu.project3;
+
+public enum OutputFormat {
+//    markdown или adoc
+
+    MARKDOWN,
+    ADOC
+}
