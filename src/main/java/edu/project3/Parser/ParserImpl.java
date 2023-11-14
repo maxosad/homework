5f6b2c0d75a2f7ad53f10e78ed8d2b9deb957227
maxosad <maxosad@mail.ru>
2023-11-14 20:21:18 +0300

    parser

--- src/main/java/edu/project3/Parser/ParserImpl.java
+++ src/main/java/edu/project3/Parser/ParserImpl.java
@@ -1,7 +1,11 @@
 package edu.project3.Parser;
 
 import edu.project3.model.LogRecord;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -10,8 +14,21 @@ public class ParserImpl implements Parser {
     @Override
     public List<LogRecord> parse(Path path) {
         try {
-            URL.of(path);
-        }
+            var uri = URI.create(path.toString());
+        } catch (IllegalArgumentException ignore) { }
+
+        Files.
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(ALL_JSON_URI))
+                .build();
+
+
+            HttpResponse<String> response =
+                client.send(request,  HttpResponse.BodyHandlers.ofString());
+
+            String[] split = response.body().split("[,\\[\\]]");
+
         return null;
     }
 }
