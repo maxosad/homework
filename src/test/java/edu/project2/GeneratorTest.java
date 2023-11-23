f0fef754edd63a2f37d6f8ce9a4d478988ff838a
maxosad <maxosad@mail.ru>
2023-11-23 18:35:21 +0300

    GeneratorTest import optimization

--- src/test/java/edu/project2/GeneratorTest.java
+++ src/test/java/edu/project2/GeneratorTest.java
@@ -2,16 +2,10 @@ package edu.project2;
 
 import edu.project2.generators.Generator;
 import edu.project2.generators.GeneratorBridge;
-import edu.project2.generators.PrimsGenerator;
-import edu.project2.model.Cell;
-import edu.project2.model.Coordinate;
 import edu.project2.model.GeneratorType;
 import edu.project2.model.Maze;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
-import java.util.List;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
