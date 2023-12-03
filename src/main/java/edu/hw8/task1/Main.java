9f158cf1d55d0c3eda645969f842728e8701f327
maxosad <maxosad@mail.ru>
2023-12-03 16:31:28 +0300

    FixedThreadPoolTest

--- src/main/java/edu/hw8/task1/Main.java
+++ src/main/java/edu/hw8/task1/Main.java
@@ -1,18 +1,18 @@
 package edu.hw8.task1;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        int port = 3435;
-        Server server = new Server(port, 2, maxConnections);
-        Client client = new Client(port);
-        Thread t1 = new Thread(() -> server.run());
-        Thread t2 = new Thread(() -> System.out.println(client.send("интеллект")));
-
-        t1.start();
-        Thread.sleep(1000);
-        t2.start();
-
-        t2.join();
-        t1.join();
-    }
+//    public static void main(String[] args) throws InterruptedException {
+//        int port = 3435;
+//        Server server = new Server(port, 2, maxConnections);
+//        Client client = new Client(port);
+//        Thread t1 = new Thread(() -> server.run());
+//        Thread t2 = new Thread(() -> System.out.println(client.send("интеллект")));
+//
+//        t1.start();
+//        Thread.sleep(1000);
+//        t2.start();
+//
+//        t2.join();
+//        t1.join();
+//    }
 }
---
+++ src/test/java/edu/hw8/task2/FixedThreadPoolTest.java
@@ -0,0 +1,43 @@
+package edu.hw8.task2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FixedThreadPoolTest {
+    private static Stream<Arguments> provideNumberOfIncrements() {
+        return Stream.of(
+            Arguments.of(1),
+            Arguments.of(10),
+            Arguments.of(100)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("execute increment test")
+    @MethodSource("provideNumberOfIncrements")
+    void execute(int expected) {
+        AtomicInteger counter = new AtomicInteger(0);
+
+        try (FixedThreadPool fixedThreadPool = FixedThreadPool.create(3)) {
+            fixedThreadPool.start();
+            for (int times = 0; times < expected; times++) {
+                fixedThreadPool.execute(counter::incrementAndGet);
+            }
+            Thread.sleep(1000);
+        } catch (Exception e ) {
+            fail(e);
+        }
+
+        assertEquals(expected, counter.get());
+
+    }
+}
