f6031b9977229e74fbf2fdde0a163bc7fc42abcb
maxosad <maxosad@mail.ru>
2023-12-03 20:36:04 +0300

    task1 many clients test

--- src/test/java/edu/hw8/ServerClientTest.java
+++ src/test/java/edu/hw8/ServerClientTest.java
@@ -3,6 +3,10 @@ package edu.hw8;
 import edu.hw8.task1.Client;
 import edu.hw8.task1.Server;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -26,5 +30,35 @@ public class ServerClientTest {
         }
 
         assertEquals("Чем ниже интеллект, тем громче оскорбления", ans);
+    }
+
+
+    @Test
+    void manyClients() {
+        int port = 3435;
+        int maxConnections = 3;
+        List<Runnable> tasks = new ArrayList<>();
+
+        try (ExecutorService executor =
+                 Executors.newFixedThreadPool(maxConnections);) {
+            Server server = new Server(port, maxConnections);
+
+            for (int clientIndex = 0; clientIndex < 10; clientIndex++) {
+                tasks.add(() -> {
+                    Client client = new Client(port);
+                    String ans = client.send("интеллект");
+                    assertEquals("Чем ниже интеллект, тем громче оскорбления", ans);
+                });
+            }
+
+            server.run();
+
+            tasks.forEach(executor::submit);
+
+            Thread.sleep(1000);
+            server.close();
+        } catch (Exception e) {
+            fail(e);
+        }
     }
 }
