cdd7c8e16371bf141a477838fc3a6bd2b42ca244
maxosad <maxosad@mail.ru>
2023-12-03 17:48:25 +0300

    task3 Hasher file with hashed passwords

---
+++ src/main/java/edu/hw8/task3/LinearPasswordChecker.java
@@ -0,0 +1,4 @@
+package edu.hw8.task3;
+
+public class LinearPasswordChecker {
+}
---
+++ src/main/java/edu/hw8/task3/PasswordChecker.java
@@ -0,0 +1,7 @@
+package edu.hw8.task3;
+
+import java.util.Map;
+
+public interface PasswordChecker {
+    Map<String, String> check(int passwordSize);
+}
