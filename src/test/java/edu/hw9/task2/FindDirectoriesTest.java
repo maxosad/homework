63d454951f5976da61af52df5ae6bdaac2cea16c
maxosad <maxosad@mail.ru>
2023-12-06 18:56:35 +0300

    separator problem

--- src/test/java/edu/hw9/task2/FindDirectoriesTest.java
+++ src/test/java/edu/hw9/task2/FindDirectoriesTest.java
@@ -1,28 +1,32 @@
 package edu.hw9.task2;
 
-import org.junit.jupiter.api.Test;
-
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.concurrent.ForkJoinPool;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FindDirectoriesTest {
 
     @Test
     void compute() {
         Path path = Path.of("src/main/java/edu/hw9/task2/files");
-        List<Path> expected = List.of(
-            Path.of("src\\main\\java\\edu\\hw9\\task2\\files\\tenFilesDir0"),
-            Path.of("src\\main\\java\\edu\\hw9\\task2\\files\\tenFilesDir1"),
-            Path.of("src\\main\\java\\edu\\hw9\\task2\\files\\tenFilesDir2"),
-            Path.of("src\\main\\java\\edu\\hw9\\task2\\files")
+        String systemSeparator = FileSystems.getDefault().getSeparator();
+        List<String[]> expectedString = List.of(
+            new String[]{"src", "main", "java", "edu", "hw9", "task2", "files", "tenFilesDir0"},
+            new String[]{"src", "main", "java", "edu", "hw9", "task2", "files", "tenFilesDir1"},
+            new String[]{"src", "main", "java", "edu", "hw9", "task2", "files", "tenFilesDir2"},
+            new String[]{"src", "main", "java", "edu", "hw9", "task2", "files"}
         );
+        List<Path> expected = expectedString.stream()
+            .map(pathNames -> Path.of(String.join(systemSeparator, pathNames)))
+            .toList();
         var recursiveTask = new FindDirectories(path);
-        
+
         try (ForkJoinPool forkJoinPool = new ForkJoinPool()) {
             var res = forkJoinPool.invoke(recursiveTask).getKey();
-
+            System.out.println(res);
             assertEquals(expected, res);
         }
 
